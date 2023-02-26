b2a3d162ee928b323a334a2c97c191cc9aad69ee
prafullajoshi970 <97543959+prafullajoshi970@users.noreply.github.com>
2023-02-26 23:50:51 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react'
+import {useState,useEffect} from 'react'
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [table,setTable]=useState([])
+  const[tableData,setTableData]=useState([])
+  
+  const[cityName,setCityName]=useState("New York")
+  const ApiFetch = () => {
+  fetch(`https://python3-dot-parul-arena-2.appspot.com/test?cityname=${cityName}`)
+      .then(resp => resp.json())
+      .then(data => (setTableData(data)))
+      .catch(error => console.error(error));
+ 
+}
+const addItem=()=>{
+  setTable([...table,tableData])}
+
+
+const addcity=(e)=>{
+    setCityName( e.target.value);
+    ApiFetch (cityName)
+    addItem();
+  }
+  const DeleteItem=(index)=>{
+    const list=[...table]
+    list.splice(index,1);
+    setTable(list);
+  }
+
+
+useEffect(() => {
+  ApiFetch();
+},[])
+console.log(table)
+console.log(tableData)
+
+
+ 
+  return (<>
+  <h3>Prafulla Joshi's Wheather App</h3>
+  <div className='App'>
+   
+   <div className='leftButtons'>
+   <button className="topleft">City</button>
+   <button className='btn1' value={"London"} onClick={ addcity}>London</button>
+   <button className='btn1' value={"New York"} onClick={ addcity}>New York</button>
+   <button className='btn1' value={"Los Angeles"} onClick={ addcity}>Los Angeles</button>
+   <button className='btn1' value={"Las Vegas"} onClick={ addcity}>Las Vegas</button>
+   </div>
+  <div className='verticalBar'></div>
+   <div className='righttableInfo'>
+     <form type={'submit'}>
+     <input type={'text'} onChange={(e)=>setCityName(e.target.value)}></input>
+     <button className='search' onClick={addcity}>search</button>
+     </form>
+    
+     <tbody>
+   <tr><th>Date&Time</th>
+   <th>Description</th>
+   <th>Pressure</th>
+   <th>Data age</th>
+   <th>Data age</th></tr>
+   
+  
+     {table.map((e,idx) =>
+     <tr key={idx}> 
+     <td>{e.date_and_time}</td>
+     <td>{e.description}</td>
+     <td>{e.humidity_in_percent}</td>
+     <td>{e.pressure_in_hPa}</td>
+     <td>{e.temp_in_celsius}</td>
+    {table.length-1===idx && <button onClick={DeleteItem}>Delete</button>}
+    </tr>
+   
+   
+ 
+    )}
+  
+   
+  
+    
+  
+   </tbody></div></div></>
+    
+  )
 }
 
-export default App;
+export default App
